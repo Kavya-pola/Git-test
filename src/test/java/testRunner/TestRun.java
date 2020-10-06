3c937b56be2e5112dea1cdc28b0ca891742b28ed
Kavya-pola <57083556+Kavya-pola@users.noreply.github.com>
2020-10-06 16:39:39 +0530

    Update TestRun.java

--- src/test/java/testRunner/TestRun.java
+++ src/test/java/testRunner/TestRun.java
@@ -14,7 +14,7 @@ import io.cucumber.junit.CucumberOptions;
 				glue="stepDefinitions",
 				dryRun=false,
 				monochrome=true,
-				plugin= {“pretty”,“html:test-output”}
+				plugin= “json:target/report-json.json”
 		)
 public class TestRun {
 	
